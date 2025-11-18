a2729962507d9d3d873f4ab54121b86603b01a3a
LuisMario87 <143245995+LuisMario87@users.noreply.github.com>
2025-11-17 22:02:04 -0600

    Update ETL Final.py

--- scripts/ETL Final.py
+++ scripts/ETL Final.py
@@ -1,20 +1,20 @@
+
 # ETL farma 
 import pandas as pd
 
-def load_data(path="C:\\Users\\luism\\OneDrive\\Documentos\\A. 9no Semestre\\Temas 2\\Procesamiento de datos\\datosFarmaceuticos30k.xlsx"):
+def load_data(path="C:\\Users\\luism\\OneDrive\\Documentos\\A. 9no Semestre\\Temas 2\\Procesamiento de datos\\datosCompletos.xlsx"):
     df = pd.read_excel(path)
     df.columns = df.columns.str.strip()
     df["FechaCompra"] = pd.to_datetime(df.get("FechaCompra"), errors="coerce")
     df["Cantidad"] = pd.to_numeric(df.get("Cantidad"), errors="coerce")
     df["PrecioUnitario"] = pd.to_numeric(df.get("PrecioUnitario"), errors="coerce")
-    df["TotalVenta"] = df["Cantidad"] * df["PrecioUnitario"]
+    df["TotalVenta"] = (df["Cantidad"] * df["PrecioUnitario"]).round(2)
     return df
 
 if __name__ == "__main__":
-    df = load_data("C:\\Users\\luism\\OneDrive\\Documentos\\A. 9no Semestre\\Temas 2\\Procesamiento de datos\\datosFarmaceuticos30k.xlsx")
+    df = load_data("C:\\Users\\luism\\OneDrive\\Documentos\\A. 9no Semestre\\Temas 2\\Procesamiento de datos\\datosCompletos.xlsx")
     print("Filas cargadas:", len(df))
      # Guardar
-    output_path = "C:\\Users\\luism\\OneDrive\\Documentos\\A. 9no Semestre\\Temas 2\\Procesamiento de datos\\datosFarmaceuticos30k.csv"
-    df.to_csv(output_path, index=False, encoding="utf-8")
-
+    output_path = "C:\\Users\\luism\\OneDrive\\Documentos\\A. 9no Semestre\\Temas 2\\Procesamiento de datos\\datosCompletosProcesados.csv"
+    df.to_csv(output_path, index=False, encoding="utf-8", float_format='%.2f')
     print("Archivo procesado guardado en:", output_path)
